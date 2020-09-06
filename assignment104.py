1a856ab2921007bf9571f9039214ea4e87680405
Shifa-Mehreen <70189185+Shifa-Mehreen@users.noreply.github.com>
2020-09-06 11:06:00 +0530

    Add files via upload

---
+++ assignment104.py
@@ -0,0 +1,14 @@
+#swapping 2 numbers
+
+#input
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+
+#logic
+temp = a #temporary variable 
+a = b
+b = temp
+
+print("Now 1st number is: ",a)
+print("And 2nd number is: ",b)
+
