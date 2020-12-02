25194f71c59d4b1edab5c2e7eab09b5c7d580ddc
Shifa-Mehreen <70189185+Shifa-Mehreen@users.noreply.github.com>
2020-12-02 22:53:57 +0530

    Add files via upload

---
+++ lab1301.py
@@ -0,0 +1,21 @@
+#121910313005
+#Shifa Mehreen
+
+#Create a Binary Tree with a single node
+
+class Node:
+	def __init__(self,data):
+		self.data = data
+		self.left = None
+		self.right = None
+class BinaryTree:
+	def __init__(self):
+		self.root = None
+
+x = int(input("Enter Root Node value: "))
+
+B = BinaryTree()
+B.root = Node(x)
+
+print("Tree : ")
+print(B.root.data)
